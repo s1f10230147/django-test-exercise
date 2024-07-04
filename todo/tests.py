dc57f017b68159ed113ec9f434b4f472da49bb6e
1F10230147 <s1f102301471@toyo.jp>
2024-07-04 13:28:15 +0900

    ソート機能を追加3

--- todo/tests.py
+++ todo/tests.py
@@ -43,7 +43,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(len(response.context["task"]),0)
+        self.assertEqual(len(response.context["tasks"]),0)
 
     def test_index_post(self):
         client=Client()
