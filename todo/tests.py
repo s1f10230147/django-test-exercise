2e222ed812d30cff0234dc2240c4735098cc196a
1F10230147 <s1f102301471@toyo.jp>
2024-07-04 13:30:00 +0900

    ソート機能を追加4

--- todo/tests.py
+++ todo/tests.py
@@ -52,7 +52,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(len(response.context["tesks"]),1)
+        self.assertEqual(len(response.context["tasks"]),1)
 
     def test_index_get_order_post(self):
         task1=Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -64,8 +64,8 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(response.context["tesks"][0],task2)
-        self.assertEqual(response.context["tesks"][1],task1)  
+        self.assertEqual(response.context["tasks"][0],task2)
+        self.assertEqual(response.context["tasks"][1],task1)  
 
     def test_index_get_order_due(self):
         task1=Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -77,5 +77,5 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(response.context["tesks"][0],task1)
-        self.assertEqual(response.context["tesks"][1],task2)  
+        self.assertEqual(response.context["tasks"][0],task1)
+        self.assertEqual(response.context["tasks"][1],task2)  
